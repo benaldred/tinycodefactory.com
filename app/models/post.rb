9f1366159344dc93bcd440d02ca5837b9e92130c
Ben Aldred <benaldred@gmail.com>
2012-02-08 23:21:31 +0000

    comment out tumblr code for now

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
-class Post
-  Tumblr.blog = 'tinycodefactory'
-  
-  def self.all(user)
-    @user = Tumblr::User.new('ben@tinycodefactory.com', 'lemon12')
-    Tumblr::Post.all(user)
-  end
-  
-  def self.find(id)
-    Tumblr::Post.find(id)
-  end
-  
-end
+#class Post
+#  Tumblr.blog = 'tinycodefactory'
+#  
+#  def self.all(user)
+#    @user = Tumblr::User.new('ben@tinycodefactory.com', 'lemon12')
+#    Tumblr::Post.all(user)
+#  end
+#  
+#  def self.find(id)
+#    Tumblr::Post.find(id)
+#  end
+#  
+#end
