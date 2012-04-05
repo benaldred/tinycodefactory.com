edc3a8551bcec6ee9ed023a4fc884e903745ada7
Ben Aldred <benaldred@gmail.com>
2012-04-05 21:18:31 +0100

    fix nva and add favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def body_class
     "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
   end
+  
+  def nav_li(title)
+    href = "##{title.downcase.parameterize('_')}"
+    href = "/#{href}" if controller.controller_name != "pages"
+    "<li><a href='#{href}'>#{title}</a></li>".html_safe
+  end
 end
