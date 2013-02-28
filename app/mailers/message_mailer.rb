8c96620ef5ace6a59f9b3ad91d5cf6b7efb62e81
Ben Aldred <benaldred@gmail.com>
2013-02-28 08:44:28 +0000

    Update the from address for filtering

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,5 @@
 class MessageMailer < ActionMailer::Base
-  default from: "ben@tinycodefactory.com"
+  default from: "contact.form@tinycodefactory.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
