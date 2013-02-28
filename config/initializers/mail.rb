34c4afc5ff0916b8a5fd2661b66c1845e3977c4f
Ben Aldred <benaldred@gmail.com>
2013-02-28 08:58:31 +0000

    WTF

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,17 +1,8 @@
-#ActionMailer::Base.smtp_settings = {
-#    :port =>           '587',
-#    :address =>        'smtp.mandrillapp.com',
-#    :user_name =>      ENV['MANDRILL_USERNAME'],
-#    :password =>       ENV['MANDRILL_APIKEY'],
-#    :domain =>         'heroku.com',
-#    :authentication => :plain
-#}
-
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
 }
