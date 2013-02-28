b41450beb1ec0cb5833be44c0f31bb377b53b998
Ben Aldred <benaldred@gmail.com>
2013-02-28 08:15:19 +0000

    see if sendgrid works

--- config/environments/mail.rb
+++ config/environments/mail.rb
@@ -1,8 +1,17 @@
+#ActionMailer::Base.smtp_settings = {
+#    :port =>           '587',
+#    :address =>        'smtp.mandrillapp.com',
+#    :user_name =>      ENV['MANDRILL_USERNAME'],
+#    :password =>       ENV['MANDRILL_APIKEY'],
+#    :domain =>         'heroku.com',
+#    :authentication => :plain
+#}
+
 ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
