6afeda578fa72d69f7c40b3b49014ed7ae26acaf
Ben Aldred <benaldred@gmail.com>
2014-03-10 10:55:52 +0000

    test to see if projects are being cached

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
   layout "projects"
-  
+  before_filter :set_http_cache_headers
+
   def show
     render :template => "projects/#{params[:id]}"
+  end
+
+  def set_http_cache_headers
+    last_modified = File.mtime("#{Rails.root}/app/views/projects/#{params[:id]}.html.erb")
+    fresh_when last_modified: last_modified , public: true, etag: last_modified
   end
 end
