012805cdbe2c5cdbfa317d6fbfa253ce57576a3e
Ben Aldred <benaldred@gmail.com>
2014-05-28 13:33:55 +0100

    Add some layout design tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 TinycodefactoryCom::Application.routes.draw do
   root :to => 'high_voltage/pages#show', :id => 'home'
-  match '/projects/:id', :to => 'projects#show', :as => 'project', :constraints => {:id => /commentarybox|boxedup|qavs|sayt|chefnote/}
+  get "/expertise" => 'high_voltage/pages#show', :id => 'services'
 
+
+  match '/projects/:id', :to => 'projects#show', :as => 'project', :constraints => {:id => /commentarybox|boxedup|qavs|sayt|chefnote/}
   post "/contact" => 'contacts#create'
 
   get '/sitemap' => 'sitemap#index'
+
 end
